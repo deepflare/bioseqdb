b2cb865cbac873303340ec042aad2d417537eb42
Mateusz Cegiełka <mateusz@cegla.net>
2022-05-12 02:26:08 +0200

    Remove old seqlib code

--- bioseqdb_pg/bwa.cpp
+++ bioseqdb_pg/bwa.cpp
@@ -2,12 +2,9 @@
 
 #include <cstring>
 #include <iostream>
-#include <sstream>
 #include <stdexcept>
 
 #include <htslib/htslib/sam.h>
-
-//#define DEBUG_BWATOOLS 1
 
 #define _set_pac(pac, l, c) ((pac)[(l)>>2] |= (c)<<((~(l)&3)<<1))
 #define _get_pac(pac, l) ((pac)[(l)>>2]>>((~(l)&3)<<1)&3)
@@ -39,34 +36,24 @@ std::string_view slice_match(std::string_view nucleotides, const uint32_t *cigar
 
 }
 
-void BioseqdbBWA::build_index(const std::vector<UnalignSequence>& v) {
-
-    if (!v.size())
-        return;
-
-    // check the integrity of the input data
-    for (std::vector<UnalignSequence>::const_iterator i = v.begin(); i != v.end(); ++i)
-        if (i->name.empty() || i->nucleotides.empty())
-            throw std::invalid_argument("BWAWrapper::constructIndex - Reference sequences must have non-empty name and seq");
-
-    if (idx) {
-        std::cerr << "...clearing old index" << std::endl;
-        bwa_idx_destroy(idx);
-        idx = 0;
-    }
+void BioseqdbBWA::build_index(const std::vector<UnalignSequence>& ref_seqs) {
+    assert(ref_seqs.size() > 0);
+    for (auto&& ref_seq : ref_seqs)
+        assert(!(ref_seq.name.empty() || ref_seq.nucleotides.empty()));
+    assert(idx == nullptr);
 
     // allocate memory for idx
     idx = (bwaidx_t*)calloc(1, sizeof(bwaidx_t));;
 
     // construct the forward-only pac
-    uint8_t* fwd_pac = seqlib_make_pac(v, true); //true->for_only
+    uint8_t* fwd_pac = seqlib_make_pac(ref_seqs, true); //true->for_only
 
     // construct the forward-reverse pac ("packed" 2 bit sequence)
-    uint8_t* pac = seqlib_make_pac(v, false); // don't write, becasue only used to make BWT
+    uint8_t* pac = seqlib_make_pac(ref_seqs, false); // don't write, becasue only used to make BWT
 
     size_t tlen = 0;
-    for (std::vector<UnalignSequence>::const_iterator i = v.begin(); i != v.end(); ++i)
-        tlen += i->nucleotides.length();
+    for (auto&& ref_seq : ref_seqs)
+        tlen += ref_seq.nucleotides.length();
 
 #ifdef DEBUG_BWATOOLS
     std::cerr << "ref seq length: " << tlen << std::endl;
@@ -85,16 +72,16 @@ void BioseqdbBWA::build_index(const std::vector<UnalignSequence>& v) {
     // make the bns
     bntseq_t * bns = (bntseq_t*) calloc(1, sizeof(bntseq_t));
     bns->l_pac = tlen;
-    bns->n_seqs = v.size();
+    bns->n_seqs = ref_seqs.size();
     bns->seed = 11;
     bns->n_holes = 0;
 
     // make the anns
-    bns->anns = (bntann1_t*)calloc(v.size(), sizeof(bntann1_t));
+    bns->anns = (bntann1_t*)calloc(ref_seqs.size(), sizeof(bntann1_t));
     size_t offset = 0;
-    for (size_t k = 0; k < v.size(); ++k) {
-        seqlib_add_to_anns(v[k].name, v[k].nucleotides, &bns->anns[k], offset);
-        offset += v[k].nucleotides.length();
+    for (size_t k = 0; k < ref_seqs.size(); ++k) {
+        seqlib_add_to_anns(ref_seqs[k].name, ref_seqs[k].nucleotides, &bns->anns[k], offset);
+        offset += ref_seqs[k].nucleotides.length();
     }
 
     //ambs is "holes", like N bases
@@ -108,37 +95,13 @@ void BioseqdbBWA::build_index(const std::vector<UnalignSequence>& v) {
 }
 
 std::vector<AlignMatch> BioseqdbBWA::align_sequence(std::string_view read_nucleotides) const {
+    assert(idx != nullptr);
 
-    // we haven't made an index, just return
-    if (!idx)
-        throw std::runtime_error("bwa index not built");
+    mem_alnreg_v ar = mem_align1(memopt, idx->bwt, idx->bns, idx->pac, read_nucleotides.length(), read_nucleotides.data()); // get all the hits (was c_str())
 
-    mem_alnreg_v ar;
-    ar = mem_align1(memopt, idx->bwt, idx->bns, idx->pac, read_nucleotides.length(), read_nucleotides.data()); // get all the hits (was c_str())
+    // TOOD: Free memory.
 
-#ifdef DEBUG_BWATOOLS
-    std::cerr << "num hits: " << ar.n << " " << name << std::endl;
-      size_t secondary_count_debug = 0;
-      for (size_t i = 0; i < ar.n; ++i) {
-	if (ar.a[i].secondary >= 0)
-	  secondary_count_debug++;
-      }
-      std::cerr << "secondary count " << secondary_count_debug << std::endl;
-    }
-#endif
-
-//    double primary_score = 0;
-//
-//    int secondary_count = 0;
-
-    //setup a vector to store the hits. Why remake a vector? Because
-    //I want them to be in the same order for all runs. With BWA multithreading,
-    //it may not be so in the initial ar.a vector.
-//    std::vector<mem_aln_t> a;
     std::vector<AlignMatch> matches;
-
-    //size_t num_secondary = 0;
-    // loop through the hits
     for (size_t i = 0; i < ar.n; ++i) {
         mem_aln_t a = mem_reg2aln(memopt, idx->bns, idx->pac, read_nucleotides.length(), read_nucleotides.data(), &ar.a[i]);
         matches.push_back({
@@ -153,81 +116,12 @@ std::vector<AlignMatch> BioseqdbBWA::align_sequence(std::string_view read_nucleo
             .cigar = cigar_compressed_to_string(a.cigar, a.n_cigar),
             .score = a.score,
         });
-
-//        if (ar.a[i].secondary >= 0 && (keep_sec_with_frac_of_primary_score < 0 || keep_sec_with_frac_of_primary_score > 1))
-//            continue; // skip secondary alignments
-
-        // get forward-strand position and CIGAR
-//        mem_aln_t a_aln;
-
-//        a_aln = mem_reg2aln(memopt, idx->bns, idx->pac, seq.length(), seq.c_str(), &ar.a[i]);
-
-//        a.push_back(a_aln);
     }
-
     return matches;
 
-    // sort it
-//    std::sort(a.begin(), a.end(), aln_sort);
-//
-//    for (size_t i = 0; i < a.size(); ++i) {
-//
-//        // if score not sufficient or past cap, continue
-//        //bool sec_and_low_score =  ar.a[i].secondary >= 0 && (primary_score * keep_sec_with_frac_of_primary_score) > a.score;
-//        //bool sec_and_cap_hit = ar.a[i].secondary >= 0 && (int)i > max_secondary;
-//        bool sec_and_low_score = (a[i].flag&BAM_FSECONDARY) && (primary_score * keep_sec_with_frac_of_primary_score) > a[i].score;
-//        bool sec_and_cap_hit =   (a[i].flag&BAM_FSECONDARY) && (int)i > max_secondary;
-//        if (sec_and_low_score || sec_and_cap_hit) {
-//            free(a[i].cigar);
-//            continue;
-//        } else if (!(a[i].flag&BAM_FSECONDARY)) {
-//            primary_score = a[i].score;
-//            //num_secondary = 0;
-//        }
-//
-//        // instantiate the read
-//        BamRecord b;
-//        b.init();
-//
-//        b.b->core.tid = a[i].rid;
-//        b.b->core.pos = a[i].pos;
-//        b.b->core.qual = a[i].mapq;
-//        b.b->core.flag = a[i].flag;
-//        b.b->core.n_cigar = a[i].n_cigar;
-//
-//        // set dumy mate
-//        b.b->core.mtid = -1;
-//        b.b->core.mpos = -1;
-//        b.b->core.isize = 0;
-//
 //        // if alignment is reverse, set it
 //        if (a[i].is_rev)
 //            b.b->core.flag |= BAM_FREVERSE;
-//
-//        std::string new_seq = seq;
-//        // if hardclip, figure out what to clip
-//        if (hardclip) {
-//            size_t tstart = 0;
-//            size_t len = 0;
-//            for (int i = 0; i < a[i].n_cigar; ++i) {
-//                if (i == 0 && bam_cigar_op(a[i].cigar[i]) == BAM_CREF_SKIP) // first N (e.g. 20N50M)
-//                    tstart = bam_cigar_oplen(a[i].cigar[i]);
-//                else if (bam_cigar_type(bam_cigar_op(a[i].cigar[i]))&1) // consumes query, but not N
-//                    len += bam_cigar_oplen(a[i].cigar[i]);
-//            }
-//            assert(len > 0);
-//            assert(tstart + len <= seq.length());
-//            new_seq = seq.substr(tstart, len);
-//        }
-//
-//        // allocate all the data
-//        b.b->core.l_qname = name.length() + 1;
-//        b.b->core.l_qseq = new_seq.length(); //(seq.length()>>1) + seq.length() % 2; // 4-bit encoding
-//        b.b->l_data = b.b->core.l_qname + (a[i].n_cigar<<2) + ((b.b->core.l_qseq+1)>>1) + (b.b->core.l_qseq);
-//        b.b.get()->data = (uint8_t*)malloc(b.b.get()->l_data);
-//
-//        // allocate the qname
-//        memcpy(b.b->data, name.c_str(), name.length() + 1);
 //
 //        // allocate the cigar. Reverse if aligned to neg strand, since mem_aln_t stores
 //        // cigars relative to referemce string oreiatnion, not forward alignment
@@ -243,10 +137,6 @@ std::vector<AlignMatch> BioseqdbBWA::align_sequence(std::string_view read_nucleo
 //            }
 //        }
 //
-//        // allocate the sequence
-//        uint8_t* m_bases = b.b->data + b.b->core.l_qname + (b.b->core.n_cigar<<2);
-//
-//        // TODO move this out of bigger loop
 //        int slen = new_seq.length();
 //        int j = 0;
 //        if (a[i].is_rev) {
@@ -287,43 +177,6 @@ std::vector<AlignMatch> BioseqdbBWA::align_sequence(std::string_view read_nucleo
 //            }
 //        }
 //
-//        // allocate the quality to NULL
-//        uint8_t* s = bam_get_qual(b.b);
-//        s[0] = 0xff;
-//
-//        b.AddIntTag("NA", ar.n); // number of matches
-//        b.AddIntTag("NM", a[i].NM);
-//
-//        if (a[i].XA)
-//            b.AddZTag("XA", std::string(a[i].XA));
-//
-//        // add num sub opt
-//        //b.AddIntTag("SB", ar.a[i].sub_n);
-//        b.AddIntTag("AS", a[i].score);
-//
-//        // count num secondaries
-//        if (b.SecondaryFlag())
-//            ++secondary_count;
-//
-//        vec.push_back(b);
-
-#ifdef DEBUG_BWATOOLS
-        // print alignment
-      printf("\t%c\t%s\t%ld\t%d\t", "+-"[a[i].is_rev], idx->bns->anns[a[i].rid].name, (long)a[i].pos, a[i].mapq);
-      for (int k = 0; k < a[i].n_cigar; ++k) // print CIGAR
-      	printf("%d%c", a[i].cigar[k]>>4, "MIDSH"[a[i].cigar[k]&0xf]);
-      printf("\t%d\n", a[i].NM); // print edit distance
-#endif
-
-//        free(a[i].cigar); // don't forget to deallocate CIGAR
-//    }
-//
-//    free (ar.a); // dealloc the hit list
-//
-//    // add the secondary counts
-//    for (BamRecordVector::iterator i = vec.begin(); i != vec.end(); ++i)
-//        i->AddIntTag("SQ", secondary_count);
-
 }
 
 // modified from bwa (heng li)
