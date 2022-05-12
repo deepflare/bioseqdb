0924d61b77427484cd273567448e94e7e3b1f113
Mateusz Cegiełka <mateusz@cegla.net>
2022-05-12 14:42:01 +0200

    Refactor seqlib_make_pac

--- bioseqdb_pg/bwa.h
+++ bioseqdb_pg/bwa.h
@@ -48,16 +48,4 @@ private:
 
     // hold the full index structure
     bwaidx_t* idx;
-
-    // overwrite the bwa bwt_pac2pwt function
-    bwt_t *seqlib_bwt_pac2bwt(const uint8_t *pac, int bwt_seq_lenr);
-
-    // add an anns (chr annotation structure)
-    bntann1_t* seqlib_add_to_anns(std::string_view name, std::string_view seq, bntann1_t * ann, size_t offset);
-
-    // overwrite the bwa-mem add1 function, which takes a sequence and adds to pac
-    uint8_t* seqlib_add1(const kseq_t *seq, bntseq_t *bns, uint8_t *pac, int64_t *m_pac, int *m_seqs, int *m_holes, bntamb1_t **q);
-
-    // make the pac structure (2-bit encoded packed sequence)
-    uint8_t* seqlib_make_pac(const std::vector<BwaSequence>& v, bool for_only);
 };
